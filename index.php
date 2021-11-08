55623e353a276f47f26a42b19830bb4eeff3fcb3
ElielAr <ariaxunach@gmail.com>
2021-11-08 17:50:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 //API INSEGURIDAD Y DELINCUENCIA
     include 'conexion.php';
+	function permisos() {  
+	  if (isset($_SERVER['HTTP_ORIGIN'])){
+		  header("Access-Control-Allow-Origin: *");
+		  header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+		  header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, Content-Type, Accept");
+		  header('Access-Control-Allow-Credentials: true');      
+	  }  
+	  if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS'){
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))          
+			header("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
+		if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
+			header("Access-Control-Allow-Headers: Origin, Authorization, X-Requested-With, Content-Type, Accept");
+		exit(0);
+	  }
+	}
+	permisos();
     $pdo = new Conexion();
     //Metodo obtener
-    if($_SERVER['REQUEST_METHOD']=='GET'){
+    if($_SERVER['REQUEST_METHOD']== 'GET'){
         if(isset($_GET['id']))
         {
             $sql=$pdo->prepare("SELECT * FROM datainde WHERE Dataid=:id");
